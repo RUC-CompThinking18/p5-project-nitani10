1dbf5128a7adc7aa1252c67a5fb991c3dafe7e0d
nitani10 <42990782+nitani10@users.noreply.github.com>
2018-12-06 22:19:30 -0500

    Commit - a working progress

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
-function setup() {
-  // put setup code here
+var distance = 1
+function setup() { // put setup code here
+createCanvas(640, 480);
+y = 50
+x = random(50,590)
+dimensions = random(30,60)
 }
-
-function draw() {
-  // put drawing code here
+function draw() { // put drawing code here
+  if (mouseIsPressed()){
+    distance = distance + 1
+  }
+clear();
+ellipse(x, y, dimensions, dimensions);
+y = y + distance;
+if (y === 480){
+  clear();
+  textSize(32)
+  text('The game is over', 40, 50)
+  noloop();
+}
 }
