476ff315f5bbbfd8f967e81ba9227488c4b053e8
nitani10 <42990782+nitani10@users.noreply.github.com>
2018-12-07 21:54:21 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -8,13 +8,16 @@ function draw() { // put drawing code here
 clear(); //clears any ellipse drawn before. Important as this code would otherwise leave the previous ellipse in the drawing when looping.
 ellipse(x, y, dimensions, dimensions);
 y = y + 1 //makes ellipse travel down the canvas at a consistent rate.
-if (y === 480){//If ellipse reaches the bottom of the canvas, the code ends
+function mouseClicked() {
+  if (mouseX < ellipseX && mouseY < ellipseY){
+    clear();
+  } else if (y === 480){//If ellipse reaches the bottom of the canvas, the code ends
   clear();
   textSize(32)
   text('The game is over', 40, 50)
   noloop();
 }
 }
-
+}
 //NOTE: I would like to change the color of the ellipse everytime that a mouse is clicked
 //NOTE: I would like the speed that the ellipse travels to increase every time that the mouse is clicked
